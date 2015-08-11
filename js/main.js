65297c14c86e08bb81aa941144e83d01e46b3096
gilnakar <g.nakar@gmail.com>
2015-08-11 15:08:03 +0300

    svg try

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  var svg = new Walkway({
+    selector: '#boxes',
+    duration: '25000',
+    easing: 'linear'
+  });
+ 
+  svg.draw();
+  
+});
+
 
